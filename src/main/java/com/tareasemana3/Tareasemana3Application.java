d8698e7fdb75e0308c0684cb4792a07c21ed0161
FaceBasu <axelpsn27@gmail.com>
2022-10-02 15:11:57 -0600

    Esta es la tarea de la semana 3

---
+++ src/main/java/com/tareasemana3/Tareasemana3Application.java
@@ -0,0 +1,13 @@
+package com.tareasemana3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tareasemana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tareasemana3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tareasemana3/Tareasemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareasemana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tareasemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
